c497d1c90f7230dfb5ace9e24bebf1940951ebc1
Manuel Duran <manuelmid99@gmail.com>
2022-12-01 21:07:30 +0100

    Working in some positions options

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-    (function() {
+(function() {
 
     var box = document.getElementById("controller");
     var botones = document.querySelectorAll("button");
@@ -7,7 +7,7 @@
     var mainContent = document.getElementById("main_content");
     var mainBox = document.getElementById("scrollable");
     var sideBar = document.getElementById("side");
-    
+
     //BODY STYLES
         box.style.backgroundColor = "rgb(0 84 166 / 30%)";
         menuBox.style.backgroundColor = "rgb(0 84 166 / 30%)";
@@ -15,26 +15,27 @@
         sideBar.style.position = "unset";
         main.style.position = "initial";
         mainBox.style = "display: flex;flex-direction: column;flex-wrap: nowrap;align-items: center;";
-    
+
         //SIZES
-            box.style.width = "600px";
-            mainContent.style.width = "580px";
-            mainContent.childNodes[1].width = "560px";
+            box.style.width = "900px";
+            mainContent.style.width = "880px";
+            mainContent.childNodes[1].width = "860px";
         //SIZES
-        
+
         //document.body.style.backgroundImage = "url(https://bestof.nyc3.digitaloceanspaces.com/devsnap.me/sam/svg-and-css-squiggly-pattern.gif)";
         document.body.style.background = "radial-gradient(circle, rgba(7,144,172,1) 0%, rgba(9,9,121,1) 62%, rgba(2,0,36,1) 100%)";
     //BODY STYLES
-    
+
     //BUTTONS
         for( let i = 0 ; i < botones.length ; i++ ){
-    
+
             botones[i].style.width = "100px";
             botones[i].style.height = "100px";
             botones[i].style.backgroundImage = "url()";
-            botones[i].style.fontSize = "30px";
-    
+            botones[i].style.fontSize = "20px";
+            botones[i].style.textAlign = "center";
+
         };
     //BUTTONS
-    
+
 })();
